e231bc029e7f75565e3243fe8d93311825cc21aa
lucaaeberhard <luca.aeberhard@gmx.net>
2022-06-23 14:04:35 +0200

    formatting and overallavg

--- src/components/GradeComponent.vue
+++ src/components/GradeComponent.vue
@@ -1,35 +1,35 @@
 <template>
-<div class="container">
-<h2 class="d-flex justify-content-center">Notenverwaltungstool</h2>
+  <div class="container">
+    <h2 class="d-flex justify-content-center">Notenverwaltungstool</h2>
 
-<h3 class="d-flex justify-content-center mb-3 mt-3">Gesamtdurchschnitt:</h3>
+    <h3 class="d-flex justify-content-center mb-3 mt-3">Gesamtdurchschnitt: {{overallAverage}}</h3>
 
-<div class="input-group mb-5 w-50 mx-auto">
-  <input type="text" v-model="newSubject" class="form-control" placeholder="New Subject">
-  <div class="input-group-append">
-    <button @click="addSubject(newSubject)" type="button" class="btn btn-primary">+</button>
-  </div>
-</div>
-
-<div class="row ml-2">
-  <ul class="col-md-3 list-group mb-5" v-for="subject in subjects">
-    <li class="list-group-item active">{{subject.name}}</li>
-    <li class="list-group-item">Durchschnitt: {{parseFloat(subject.average).toFixed(2)}}</li>
-    <li class="list-group-item" v-for="(grades, index) in subject.grades">
-      {{parseFloat(grades).toFixed(2)}}
-      <button @click="deleteGrade(subject.name, index)" type="button" class="btn btn-danger m-3">X</button>
-    </li>
-    <li class="list-group-item">
-      <div class="input-group">
-      <input type="number" v-model="newGrade" class="form-control">
+    <div class="input-group mb-5 w-50 mx-auto">
+      <input type="text" v-model="newSubject" class="form-control" placeholder="New Subject">
       <div class="input-group-append">
-        <button @click="addGrade(newGrade, subject.name)" type="button" class="btn btn-primary">+</button>
+        <button @click="addSubject(newSubject)" type="button" class="btn btn-primary">+</button>
       </div>
     </div>
-    </li>
-  </ul>
-</div>
-</div>
+
+    <div class="row ml-2">
+      <ul class="col-md-3 list-group mb-5" v-for="subject in subjects">
+        <li class="list-group-item active">{{subject.name}}</li>
+        <li class="list-group-item">Durchschnitt: {{parseFloat(subject.average).toFixed(2)}}</li>
+        <li class="list-group-item" v-for="(grades, index) in subject.grades">
+          {{parseFloat(grades).toFixed(2)}}
+          <button @click="deleteGrade(subject.name, index)" type="button" class="btn btn-danger m-3">X</button>
+        </li>
+        <li class="list-group-item">
+          <div class="input-group">
+            <input type="number" v-model="newGrade" class="form-control">
+            <div class="input-group-append">
+              <button @click="addGrade(newGrade, subject.name)" type="button" class="btn btn-primary">+</button>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
   
 </template>
 
@@ -52,7 +52,8 @@ export default {
         {name: "WUR", grades: [5.00, 6.00], average: 0.00},
       ],
       newSubject: "",
-      newGrade: ""
+      newGrade: "",
+      overallAverage: ""
     }
   },
   methods: {
@@ -62,16 +63,17 @@ export default {
       }
     },
     addGrade: function(newGrade, subjectName) {
-        if (newGrade.toString().split('.')[0].length == 1 && newGrade.toString().split('.')[0] <= 6 && newGrade.toString().split('.')[0] > 0) {
-          this.subjects[this.subjects.findIndex((sub) => sub.name == subjectName)].grades.push(newGrade.toFixed(2));
-          this.newGrade = "";
-          this.updateAllAverages();
+      if(newGrade.toString().split('.')[0].length == 1 && newGrade.toString().split('.')[0] <= 6 && newGrade.toString().split('.')[0] > 0) {
+        this.subjects[this.subjects.findIndex((sub) => sub.name == subjectName)].grades.push(newGrade.toFixed(2));
+        this.newGrade = "";
+        this.updateAllAverages();
+        this.updateOverAllAverage();
       }
-      
     },
     deleteGrade: function(subjectName, index) {
       this.subjects[this.subjects.findIndex((sub) => sub.name == subjectName)].grades.splice(index, 1);
       this.updateAllAverages();
+      this.updateOverAllAverage();
     },
     updateAllAverages: function() {
       this.subjects.forEach(subject => {
@@ -81,10 +83,18 @@ export default {
         })
         this.subjects[this.subjects.indexOf(subject)].average = sum / this.subjects[this.subjects.indexOf(subject)].grades.length;
       })
+    },
+    updateOverAllAverage: function() {
+      let sum = 0.00;
+      this.subjects.forEach(subject => {
+        sum = parseFloat(sum.toString()) + parseFloat(subject.average.toString());
+      })
+      this.overallAverage = sum / this.subjects.length;
     }
   },
   beforeMount() {
     this.updateAllAverages();
+    this.updateOverAllAverage();
   }
 }
 </script>
